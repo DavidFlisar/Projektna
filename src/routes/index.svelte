6d3153e649d39d1dba6e4ceed9c276f5e2250981
DavidFlisar <103906247+DavidFlisar@users.noreply.github.com>
2024-01-22 16:05:55 +0100

    Add files via upload

---
+++ src/routes/index.svelte
@@ -0,0 +1,26 @@
+<script context="module">
+    export async function load({fetch}){
+        const res = await fetch(
+            `https://api.themoviedb.org/3/movie/popular?api_key=2657f6d4bcde67e39184ed1bfed8edd6&language=en-US&page=1`
+        );
+        const data = await res.json();
+        console.log(data);
+        if(res.ok){
+            return{
+                props:{popular: data.results}
+            };
+        }
+    }
+</script>
+
+<script>
+    import PopularMovies from '../components/PopularMovies.svelte';
+    import SearchMovies from '../components/SearchMovies.svelte';
+    export let popular;
+    import {fly} from 'svelte/transition';
+</script>
+
+<section in:fly={{y:50,duration:500,delay:500}} out:fly={{duration:500}}>
+    <SearchMovies />
+    <PopularMovies {popular}/>
+</section>
